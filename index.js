8b7e4987a914b2dc3012f49e854e6d8385f8ea65
Hazar Saad ElDine <saadeldinehazar@gmail.com>
2021-05-04 11:35:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+function createEmployeeRecord(array){
+    let newArray
+    return newArray = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+function createEmployeeRecords(ar){
+    return ar.map(createEmployeeRecord)
+}
+/////////////////////////////////////////////////
+
+function createDSObj(object, dateStamp) {
+    return {type: object, date: dateStamp.slice(0,10), hour: parseInt(dateStamp.slice(-4))}
+}
+
+/////////////////////////////////////////////////
